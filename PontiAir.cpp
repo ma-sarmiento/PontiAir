6aabaff6a2a4d0eb159271a6ee261182033b3c6e
Miguel Sarmiento <ma_sarmiento@javeriana.edu.co>
2025-11-08 02:08:32 -0500

    Funcionalidad 1: Comprar y cotizar tiquetes

--- PontiAir.cpp
+++ PontiAir.cpp
@@ -57,3 +57,240 @@ int main (){
         }
     }while(repetirM);
 }
+void comprar(avion aviones[]){
+	char tamAvionC;
+	int numVueloC;
+	int numSillaC;
+	int agregar=1;
+	int agregarP=1;
+	do{
+		cout<<"Digite el tipo de avion en mayusculas, para salir digite otra letra: ";
+		cin>>tamAvionC;
+		switch(tamAvionC){
+			case 'P':
+			agregarP=1;
+			cout<<"Ingrese el numero de vuelo (1 a 5): ";
+			cin>>numVueloC;
+			while(numVueloC < 1 || numVueloC > 5){
+				cout<<"Ingrese el numero de vuelo (1 a 5): ";
+				cin>>numVueloC;
+			}
+			aviones[numVueloC-1].tamAvion = 'P';
+			aviones[numVueloC-1].numVuelo = numVueloC;
+			do{
+				cout<<"Ingrese la silla que desea de 1 a 16: ";
+				cin>>numSillaC;
+				while(numSillaC>16 || aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+					if(aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+						cout<<"La silla esta ocupada, escoja otra: ";
+					}
+					cin>>numSillaC;
+				}
+				aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada=true;
+				aviones[numVueloC-1].pasajeros[numSillaC-1].numSillaP = numSillaC;
+				if(numSillaC>=1 && numSillaC<=8){
+					aviones[numVueloC-1].asientoPref+=1;
+				}else if(numSillaC>=9 && numSillaC<=16){
+					aviones[numVueloC-1].asientoGeneral+=1;
+				}
+				cout<<"Nombre: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].nombre;
+				cout<<"Apellido: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].apellido;
+				cout<<"Edad: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].edad;
+				cout<<"Sexo (M para masculino, F para femenino en mayusculas): ";
+				do{
+					cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].sexo;
+				}while(aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!='M' && aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!= 'F');
+				aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla=cotizar(numSillaC, 'P', aviones[numVueloC-1].pasajeros[numSillaC-1].edad);
+				cout<<"El precio de la sillas es: "<<aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla<<endl;
+				cout<<endl;
+				//cotizacion del asiento por cada tipo de avion
+				cout<<"- COTIZAZIONES - "<<endl;
+				cout<<"El precio de la silla en el avion pequenio es de: "<<aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla<<endl;
+				cout<<"El precio de la silla en el avion mediano es de: "<<cotizar(numSillaC, 'M', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				cout<<"El precio de la silla en el avion grande es de: "<<cotizar(numSillaC, 'G', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				cout<<"Si desea ingresar una persona mas a este numero de vuelo digite 1"<<endl;
+				cin>>agregarP;
+			}while(agregarP==1);
+			break;
+			case 'M':
+			agregarP=1;
+			cout<<"Ingrese el numero de vuelo (6 a 10): ";
+			cin>>numVueloC;
+			while(numVueloC < 6 || numVueloC > 10){
+				cout<<"Ingrese el numero de vuelo (6 a 10): ";
+				cin>>numVueloC;
+			}
+			aviones[numVueloC-1].tamAvion = 'M';
+			aviones[numVueloC-1].numVuelo = numVueloC;
+			do{
+				cout<<"Ingrese la silla que desea de 1 a 28: ";
+				cin>>numSillaC;
+				while(numSillaC>28 || aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+					if(aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+						cout<<"La silla esta ocupada, escoja otra: ";
+					}
+					cin>>numSillaC;
+				}
+				aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada=true;
+				aviones[numVueloC-1].pasajeros[numSillaC-1].numSillaP = numSillaC;
+				if(numSillaC>=1 && numSillaC<=8){
+					aviones[numVueloC-1].asientoPref+=1;
+				}else if(numSillaC>=9 && numSillaC<=28){
+					aviones[numVueloC-1].asientoGeneral+=1;
+				}
+				cout<<"Nombre: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].nombre;
+				cout<<"Apellido: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].apellido;
+				cout<<"Edad: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].edad;
+				cout<<"Sexo (M para masculino, F para femenino en mayusculas): ";
+				do{
+					cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].sexo;
+				}while(aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!='M' && aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!= 'F');
+				aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla=cotizar(numSillaC, 'M', aviones[numVueloC-1].pasajeros[numSillaC-1].edad);
+				cout<<"El precio de la sillas es: "<<aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla<<endl;
+				cout<<endl;
+				//cotizaciones de los precios de la silla en cada tipo de avion
+				cout<<"- COTIZAZIONES - "<<endl;
+				if(numSillaC<=16){
+					cout<<"El precio de la silla en el avion pequenio es de: "<<cotizar(numSillaC, 'P', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				}else if(numSillaC>16){
+					cout<<"Esta silla no existe en el avion pequenio"<<endl;
+				}
+				cout<<"El precio de la silla en el avion mediano es de: "<<aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla<<endl;
+				cout<<"El precio de la silla en el avion grande es de: "<<cotizar(numSillaC, 'G', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				cout<<"Si desea ingresar una persona mas a este numero de vuelo digite 1"<<endl;
+				cin>>agregarP;
+			}while(agregarP==1);
+			break;
+			case 'G':
+			agregarP=1;
+			cout<<"Ingrese el numero de vuelo (11 a 15): ";
+			cin>>numVueloC;
+			while(numVueloC < 11 || numVueloC > 15){
+				cout<<"Ingrese el numero de vuelo (11 a 15): ";
+				cin>>numVueloC;
+			}
+			aviones[numVueloC-1].tamAvion = 'G';
+			aviones[numVueloC-1].numVuelo = numVueloC;
+			do{
+				cout<<"Ingrese la silla que desea de 1 a 40: ";
+				cin>>numSillaC;
+				while(numSillaC>40|| aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+					if(aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada == true){
+						cout<<"La silla esta ocupada, escoja otra: ";
+					}
+					cin>>numSillaC;
+				}
+				aviones[numVueloC-1].pasajeros[numSillaC-1].ocupada=true;
+				aviones[numVueloC-1].pasajeros[numSillaC-1].numSillaP = numSillaC;
+				if(numSillaC>=1 && numSillaC<=8){
+					aviones[numVueloC-1].asientoPref+=1;
+				}else if(numSillaC>=9 && numSillaC<=40){
+					aviones[numVueloC-1].asientoGeneral+=1;
+				}
+				cout<<"Nombre: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].nombre;
+				cout<<"Apellido: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].apellido;
+				cout<<"Edad: ";
+				cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].edad;
+				cout<<"Sexo (M para masculino, F para femenino es mayusculas): ";
+				do{
+					cin>>aviones[numVueloC-1].pasajeros[numSillaC-1].sexo;
+				}while(aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!='M' && aviones[numVueloC-1].pasajeros[numSillaC-1].sexo!= 'F');
+				aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla=cotizar(numSillaC, 'G', aviones[numVueloC-1].pasajeros[numSillaC-1].edad);
+				cout<<"El precio de la sillas es: "<<aviones[numVueloC-1].pasajeros[numSillaC-1].precioSilla<<endl;
+				cout<<endl;
+				//Cotizaciones del asiento por cada tipo de avion
+				cout<<"- COTIZAZIONES - "<<endl;
+				if(numSillaC<=16){
+					cout<<"El precio de la silla en el avion pequenio es de: "<<cotizar(numSillaC, 'P', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				}else if(numSillaC>16){
+					cout<<"Esta silla no existe en el avion pequenio"<<endl;
+				}
+				if(numSillaC<=28){
+					cout<<"El precio de la silla en el avion mediano es de: "<<cotizar(numSillaC, 'M', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				}else if(numSillaC>28){
+					cout<<"Esta silla no existe en el avion mediano"<<endl;
+				}
+				cout<<"El precio de la silla en el avion grande es de: "<<cotizar(numSillaC, 'G', aviones[numVueloC-1].pasajeros[numSillaC-1].edad)<<endl;
+				cout<<"Si desea ingresar una persona mas a este numero de vuelo digite 1"<<endl;
+				cin>>agregarP;
+			}while(agregarP==1);
+			break;
+			default:
+			agregar=0;
+			break;
+		}
+	}while(agregar==1);
+}
+long cotizar(int numSilla, char tamAvion, int edad){
+	long precio=0;
+	switch(tamAvion){
+		case 'P':
+			if(numSilla>=1 && numSilla<=8){
+				precio=120000;
+				if(edad>=1 && edad<=5){
+					precio= 120000 - (120000*0.08);
+				}
+				else if(edad>60){
+					precio= 120000 - (120000*0.05);
+				}
+			}else if(numSilla>=9 && numSilla<=16){
+				precio=30000;
+				if(edad>=1 && edad<=5){
+					precio= 30000 - (30000*0.15);
+				}
+				else if(edad>60){
+					precio= 30000 - (30000*0.1);
+				}
+			}
+			return precio;
+			break;
+		case 'M':
+			if (numSilla>=1 && numSilla<=8){
+				precio=150000;
+				if(edad>=1 && edad <=5){
+					precio=150000 - (150000 * 0.08);
+				}
+				else if (edad>60){
+					precio=150000 - (150000 * 0.05);
+				}
+			}
+			else if(numSilla>=9 && numSilla <=28){
+				precio=35000;
+				if(edad>=1 && edad <=8){
+					precio= 35000 - (35000*0.15);
+				}else if(edad>60){
+					precio=35000 - (35000*0.1);
+				}
+			}
+			return precio;
+			break;
+		case 'G':
+			if (numSilla>=1 && numSilla<=8){
+				precio=200000;
+				if(edad>=1 && edad <=5){
+					precio=200000 - (200000 * 0.08);
+				}
+				else if (edad>60){
+					precio=200000 - (200000 * 0.05);
+				}
+			}
+			else if(numSilla>=9 && numSilla <=40){
+				precio=40000;
+				if(edad>=1 && edad <=5){
+					precio= 40000 - (40000*0.15);
+				}else if(edad>60){
+					precio=40000 - (40000*0.1);
+				}
+			}
+			return precio;
+			break;
+	}
+}
